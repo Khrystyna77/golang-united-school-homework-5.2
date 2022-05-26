f4c439f905257d5a592a14ebc85b023f946b1667
Khrystyna2409 <xrustuna.di@gmail.com>
2022-05-26 19:36:21 +0300

    1

--- cache.go
+++ cache.go
@@ -1,16 +1,38 @@
 package cache
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+type Identify struct {
+	value string
+}
 
 type Cache struct {
+	cashe map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{cashe: make(map[string]Value)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+//func (receiver) Get(key string) (string, bool) {
 
+//}
+func (b *Cas) Get(key string) (string, bool) {
+
+	v, ok := b.cashall[key]
+	if ok == true {
+		fmt.Println(v)
+		return v.value, ok
+	}
+	if ok == false {
+		err1 := "not found this value"
+		fmt.Println(err1)
+
+	}
+	return "", false
 }
 
 func (receiver) Put(key, value string) {
